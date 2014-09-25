3dc50f17c3bd3cb09ea65bee6d76617937f35611
cj360 <ayunker551@gmail.com>
2014-09-24 22:54:47 -0400

    Stop Led turning off & on when charging
    By removing an led check
    
    Signed-off-by: cj360 <ayunker551@gmail.com>
    Change-Id: Iea5eae7d2ee4122a85ee3e61f3ec9b166dee26c5

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1392,7 +1392,7 @@ public class NotificationManagerService extends INotificationManager.Stub
             boolean queryRemove = false;
             boolean packageChanged = false;
             boolean cancelNotifications = true;
-            
+
             if (action.equals(Intent.ACTION_PACKAGE_ADDED)
                     || (queryRemove=action.equals(Intent.ACTION_PACKAGE_REMOVED))
                     || action.equals(Intent.ACTION_PACKAGE_RESTARTED)
@@ -1925,7 +1925,7 @@ public class NotificationManagerService extends INotificationManager.Stub
         enqueueNotificationInternal(pkg, basePkg, Binder.getCallingUid(), Binder.getCallingPid(),
                 tag, id, notification, idOut, userId);
     }
-    
+
     private final static int clamp(int x, int low, int high) {
         return (x < low) ? low : ((x > high) ? high : x);
     }
@@ -2254,8 +2254,6 @@ public class NotificationManagerService extends INotificationManager.Stub
                     if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != 0
                             && canInterrupt) {
                         mLights.add(r);
-                        // force reevaluation of active light
-                        mLedNotification = null;
                         updateLightsLocked();
                     } else {
                         if (old != null
