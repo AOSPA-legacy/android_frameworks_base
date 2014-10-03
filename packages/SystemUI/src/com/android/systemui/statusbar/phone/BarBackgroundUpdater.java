8b6a5bb85c2b8514dbae7c64df3a9d3f2387be04
Valter Strods <valterstrods@aospa.co>
2014-10-03 22:08:30 +0300

    Merge DSB icon color picking across bar types
    Fixes AOSPA-860 (JIRA).
    
    This commit gets rid of the special cases for translucent bars as
    people are not happy with white icons on white backgrounds.
    
    Change-Id: Ia926e623e8a5d25af6321cf2c6817d8881774e2f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BarBackgroundUpdater.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BarBackgroundUpdater.java
@@ -146,9 +146,8 @@ public class BarBackgroundUpdater {
                                 (0.299f * Color.red(statusBarOverrideColor) +
                                 0.587f * Color.green(statusBarOverrideColor) +
                                 0.114f * Color.blue(statusBarOverrideColor)) / 255;
-                        final boolean isStatusBarConsistent = colors[1] == 1;
-                        updateStatusBarIconColor(statusBarBrightness > 0.7f &&
-                                isStatusBarConsistent ? 0x95000000 : 0xFFFFFFFF);
+                        updateStatusBarIconColor(statusBarBrightness > 0.7f ?
+                                0x95000000 : 0xFFFFFFFF);
                     } else {
                         // dynamic status bar is disabled
                         updateStatusBarColor(0);
@@ -156,7 +155,7 @@ public class BarBackgroundUpdater {
                     }
 
                     if (mNavigationEnabled) {
-                        final int navigationBarOverrideColor = colors[2];
+                        final int navigationBarOverrideColor = colors[1];
                         updateNavigationBarColor(navigationBarOverrideColor);
 
                         // magic from http://www.w3.org/TR/AERT#color-contrast
@@ -164,9 +163,8 @@ public class BarBackgroundUpdater {
                                 (0.299f * Color.red(navigationBarOverrideColor) +
                                 0.587f * Color.green(navigationBarOverrideColor) +
                                 0.114f * Color.blue(navigationBarOverrideColor)) / 255;
-                        final boolean isNavigationBarConsistent = colors[3] == 1;
-                        updateNavigationBarIconColor(navigationBarBrightness > 0.7f &&
-                                isNavigationBarConsistent ? 0x95000000 : 0xFFFFFFFF);
+                        updateNavigationBarIconColor(navigationBarBrightness > 0.7f ?
+                                0x95000000 : 0xFFFFFFFF);
                     } else {
                         // dynamic navigation bar is disabled
                         updateNavigationBarColor(0);
